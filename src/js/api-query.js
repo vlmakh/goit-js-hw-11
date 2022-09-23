cc529b830f60e2650fb55f5a2c822af83fcbd71c
vlmakh <vlmakh@gmail.com>
2022-09-23 14:58:07 +0300

    add using axios

--- src/js/api-query.js
+++ src/js/api-query.js
@@ -1,6 +1,10 @@
-const MAIN_URL =
+import axios from 'axios';
+
+axios.defaults.baseURL =
   'https://pixabay.com/api/?key=29727763-9de4927242ac493db1fc7e125&image_type=photo&orientation=horizontal&safesearch=true';
+
 export const perPage = 24;
+
 export default class ApiQuery {
   constructor() {
     this.searchQuery = '';
@@ -8,22 +12,22 @@ export default class ApiQuery {
   }
 
   fetchImages() {
-    return fetch(
-      `${MAIN_URL}&q=${this.searchQuery}&page=${this.page}&per_page=${perPage}`
-    )
-      .then(response => response.json())
-      .then(data => {
+    // return fetch(
+    //   `${MAIN_URL}&q=${this.searchQuery}&page=${this.page}&per_page=${perPage}`
+    // )
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     this.increasePageNum();
+    //     return data;
+    //   });
+
+    return axios
+      .get(`&q=${this.searchQuery}&page=${this.page}&per_page=${perPage}`)
+      .then(response => {
         this.increasePageNum();
+        const data = response.data;
         return data;
       });
-  }
-
-  get query() {
-    return this.searchQuery;
-  }
-
-  set query(newQuery) {
-    this.searchQuery = newQuery;
   }
 
   increasePageNum() {
